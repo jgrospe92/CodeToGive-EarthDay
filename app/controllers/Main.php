916ac67ca360cf09fa704f7a503e050e7737d7a8
Md Saqliyan Islam <82405606+SaqSaq815@users.noreply.github.com>
2022-11-06 14:01:09 -0500

    removed test commands
    removed th etest functions from main.

--- app/controllers/Main.php
+++ app/controllers/Main.php
@@ -5,23 +5,4 @@ class Main extends \app\core\Controller {
     public function index() {
         $this->view('Main/index');
     }
-
-
-
-    public function gleaner_Profile(){
-        $this->view('Gleaner/gleaner_Profile');
-    }
-
-    public function farmer_Profile(){ 
-        $this->view('Farmer/farmer_Profile');
-    }
-
-    public function gleanerLogin(){ 
-        $this->view('Login/gleanerLogin');
-    }
-
-    public function foodBank_Profile(){ 
-        $this->view('FoodBank/foodBank_Profile');
-    }
-
 }
