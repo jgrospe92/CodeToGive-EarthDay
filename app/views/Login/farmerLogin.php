9f197f97f20b18a4fdc145dd5a536cc81f9481e5
Md Saqliyan Islam <82405606+SaqSaq815@users.noreply.github.com>
2022-11-05 23:15:40 -0400

    update login pages
    added way to navigate from one login page to another

--- app/views/Login/farmerLogin.php
+++ app/views/Login/farmerLogin.php
@@ -30,7 +30,7 @@
                   <img src="\assets\ugleen.png" width="80" height="80" />
                 </a>
                 <a href="/">
-                  <img src="\assets\ugleen_txt.png" width="40" height="40" />
+                  <img src="\assets\ugleen_txt.png" width="80" height="80" />
                 </a>
               </div>
             </nav>
@@ -41,6 +41,13 @@
                     <div class="col-md-5">
                         <div class="card">
                             <h2 class="card-title text-center">Login as Farmer</a></h2>
+
+                            <div class="navbar">
+                                <a href="\app\views\Login\gleanerLogin.php">Gleaner</a>
+                                <a class="active" href="\app\views\Login\farmerLogin.php">Famer</a>
+                                <a href="\app\views\Login\foodBankLogin.php">Food Bank</a>
+                            </div>
+
                             <hr >
                                 <div class="card-body py-md-4">
                                     <form _lpchecked="1" action='' method='post'>
@@ -100,6 +107,45 @@
             background-color: #00D100;
             border-color: #00D100;
             transition: .3s;
+        }
+
+
+        /* Style the navigation menu */
+        .navbar {
+            width: 100%;
+            background-color: rgb(255, 255, 255);
+            overflow: auto;
+        }
+
+        /* Navigation links */
+        .navbar a {
+            float: left;
+            padding: 10px;
+            color: black;
+            text-decoration: none;
+            font-size: 17px;
+            width: 33.33%; /* Four equal-width links. If you have two links, use 50%, and 33.33% for three links, etc.. */
+            text-align: center; /* If you want the text to be centered */
+        }
+
+        /* Add a background color on mouse-over */
+        .navbar a:hover {
+            background-color: rgb(240, 240, 240);
+        }
+
+        /* Style the current/active link */
+        .navbar a.active {
+            background-color: rgb(230, 230, 230);
+        }
+
+        /* Add responsiveness - on screens less than 500px, make the navigation links appear on top of each other, instead of next to each other */
+        @media screen and (max-width: 500px) {
+        .navbar a {
+            float: none;
+            display: block;
+            width: 100%;
+            text-align: left; /* If you want the text to be left-aligned on small screens */
+        }
         }
     </style>
 </html>
