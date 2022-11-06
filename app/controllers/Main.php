86bc57d0ca09253bef8d241d03ded67956e089d2
PhucNg26 <ndtphuccc@gmail.com>
2022-11-06 18:20:14 -0500

    Main.php
    fixed Main

--- app/controllers/Main.php
+++ app/controllers/Main.php
@@ -5,4 +5,31 @@ class Main extends \app\core\Controller {
     public function index() {
         $this->view('Main/index');
     }
+    public function gleaner_Profile_edit(){
+        $this->view('Gleaner/gleaner_Profile_edit');
+    }
+
+    public function farmer_Profile_edit(){ 
+        $this->view('Farmer/farmer_Profile_edit');
+    }
+    public function farmer_Profile(){ 
+        $this->view('Farmer/farmer_Profile');
+    }
+
+    public function foodBank_Profile_edit(){
+        $this->view('FoodBank/foodBank_Profile_edit');
+    }
+    public function foodBank_Profile(){
+        $this->view('FoodBank/foodBank_Profile');
+    }
+
+    public function farmerLogin(){ 
+        $this->view('Login/farmerLogin');
+    }
+    public function foodBankLogin(){ 
+        $this->view('Login/foodBankLogin');
+    }
+    public function gleanerLogin(){ 
+        $this->view('Login/gleanerLogin');
+    }
 }
