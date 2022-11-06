2464b6e019225ff7b025e93240b933917ca0618e
PhucNg26 <ndtphuccc@gmail.com>
2022-11-06 15:08:13 -0500

    createEvent.php
    added the skeleton for creating an event UI

---
+++ app/views/Events/createEvents.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php $this->view('Layout/Header'); ?>
+    <body>
+    <?php $this->view('Layout/Navigation'); ?>
+        <main>
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-5">
+                        <div class="card">
+                            <h2 class="card-title text-center">Creating your event</a></h2>
+                            <hr >
+                                <div class="card-body py-md-4">
+                                    <form _lpchecked="1" action='' method='post'>
+                                        <div class="form-group">
+                                            <input type="text" class="form-control" id="name" placeholder="Name">
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="text" class="form-control" id="info" placeholder="Information">
+                                        </div>     
+                                        <div class="form-group">
+                                        <label for="status">Urgency</label>
+                                            <select id="status" class="form-control">
+                                                <option selected>Choose...</option>
+                                                <option>Critical</option>
+                                                <option>Urgence</option>
+                                                <option>Demanded</option>
+                                            </select>
+                                        </div>
+                                        <label for="status">Product type</label>
+                                            <select id="status" class="form-control">
+                                                <option selected>Choose...</option>
+                                                <option>...</option>
+                                            </select>
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="text" class="form-control" id="productquantity" placeholder="Product quantity (kg)">
+                                        </div>
+                                        <div class="d-flex flex-row align-items-center justify-content-between">
+                                            <a href="\app\views\Login\gleaner_feed.php">Cancel</a>
+                                            <button type= "submit" class="btn btn-primary">Create a gleaning event</button>
+                                        </div>
+                                    </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+        <?php $this->view('Layout/Footer'); ?>
+        <?php $this->view('Layout/Scripts'); ?>
+    </body>
+
+    <style>
+        @import url('https://fonts.googleapis.com/css?family=PT+Sans');
+
+        body{
+            background: #fff;
+            font-family: 'PT Sans', sans-serif;
+        }
+        h2{
+            padding-top: 1.5rem;
+        }
+        .card{
+            border: 0.40rem solid #f8f9fa;
+            top: 10%;
+        }
+        .form-control{
+            background-color: #f8f9fa;
+            padding: 20px;
+            padding: 25px 15px;
+            margin-bottom: 1.3rem;
+        }
+
+        .form-control:focus {
+            color: #000000;
+            background-color: #ffffff;
+            border: 3px solid #da5767;
+            outline: 0;
+            box-shadow: none;
+        }
+        .btn{
+            padding: 0.6rem 1.2rem;
+            background: #50c2d1;
+            border: 2px solid #50c2d1;
+        }
+        .btn-primary:hover {
+            background-color: #00D100;
+            border-color: #00D100;
+            transition: .3s;
+        }
+    </style>
+</html>
