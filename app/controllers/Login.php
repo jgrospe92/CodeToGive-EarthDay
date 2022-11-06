f6f0a282d96b345bb946cf2640ef0c5d01d4459a
Kaolin <79109827+KaolinKiiro@users.noreply.github.com>
2022-11-06 10:30:13 -0500

    Fixed a lot of parts

---
+++ app/controllers/Login.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace app\controllers;
+
+class Login extends \app\core\Controller
+{
+    public function gleaner() {
+        $this->view('Login/gleanerLogin');
+    }
+    public function farmer() {
+        $this->view('Login/farmerLogin');
+    }
+    public function foodbank() {
+        $this->view('Login/foodbankLogin');
+    }
+}
--- app/controllers/Register.php
+++ app/controllers/Register.php
@@ -4,7 +4,13 @@ namespace app\controllers;
 class Register extends \app\core\Controller {
     
     public function farmer(){
-        $this->view('Register/farmer');
+        $this->view('Register/farmerRegister');
+    }
+    public function gleaner(){
+        $this->view('Register/gleanerRegister');
+    }
+    public function foodbank(){
+        $this->view('Register/foodbankRegister');
     }
    
 }
