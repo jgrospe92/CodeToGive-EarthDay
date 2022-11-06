d21afbb19953f979ac8aacefb3085be005d1a25b
Karim Jabbour <kareemjabbour@gmail.com>
2022-11-06 10:55:58 -0500

    Revert "elon_gone"
    This reverts commit 2b19fd9326cd239d46a193de8e5db7968c50b701.

--- app/core/autoload.php
+++ app/core/autoload.php
@@ -2,9 +2,9 @@
 
 spl_autoload_register(
 	// if you're using mac, comment this part
-	// function ($class_name){
-	// 	require_once($class_name . ".php");
-	// }
+	function ($class_name){
+		require_once($class_name . ".php");
+	}
 	
 	
 );
