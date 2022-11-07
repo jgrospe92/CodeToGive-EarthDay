ad607ecf1707dcdedf38064b21e3c6dc5c7c3a1a
Jeffrey <jeffreygrospe@yahoo.com>
2022-11-06 21:08:46 -0500

    added home page link
    added home page link to the nav bar

--- app/views/Layout/Navigation.php
+++ app/views/Layout/Navigation.php
@@ -1,11 +1,10 @@
+
 <header>
     <nav>
         <img src="/resources/images/earthday_white.png" style="cursor:pointer" onclick="window.location.href='https:/earthday.ca/'" alt="">
 
         <img class="uglean-logo" src="/resources/images/uglean-logo.png" alt="">
         <div>
-
-
             <div id="menuToggle">
                 <input type="checkbox" />
                 <span></span>
@@ -13,7 +12,7 @@
                 <span></span>
 
                 <ul id="menu">
-                    <a href="#">
+                    <a href="/Main/index">
                         <li>Home</li>
                     </a>
                     <a href="#">
