753556858b3e789b5444f93be284e1f9b5a58cd7
Md Saqliyan Islam <82405606+SaqSaq815@users.noreply.github.com>
2022-11-07 00:10:04 -0500

    worked on create event page
    worked on the create event page

--- app/controllers/Main.php
+++ app/controllers/Main.php
@@ -8,6 +8,7 @@ class Main extends \app\core\Controller {
 
 
 
+    // saq's test functions to load pages
     public function gleaner_Profile_edit(){
         $this->view('Gleaner/gleaner_Profile_edit');
     }
@@ -34,5 +35,9 @@ class Main extends \app\core\Controller {
     }
     public function gleanerLogin(){ 
         $this->view('Login/gleanerLogin');
+    }
+
+    public function createEvents(){ 
+        $this->view('Events/createEvents');
     }
 }
--- app/views/Events/createEvents.php
+++ app/views/Events/createEvents.php
@@ -1,93 +1,126 @@
 <!DOCTYPE html>
-<html lang="en">
-<?php $this->view('Layout/Header'); ?>
-    <body>
-    <?php $this->view('Layout/Navigation'); ?>
-        <main>
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-md-5">
-                        <div class="card">
-                            <h2 class="card-title text-center">Creating your event</a></h2>
-                            <hr >
-                                <div class="card-body py-md-4">
-                                    <form _lpchecked="1" action='' method='post'>
-                                        <div class="form-group">
-                                            <input type="text" class="form-control" id="name" placeholder="Name">
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="text" class="form-control" id="info" placeholder="Information">
-                                        </div>     
-                                        <div class="form-group">
-                                        <label for="status">Urgency</label>
-                                            <select id="status" class="form-control">
-                                                <option selected>Choose...</option>
-                                                <option>Critical</option>
-                                                <option>Urgence</option>
-                                                <option>Demanded</option>
-                                            </select>
-                                        </div>
-                                        <label for="status">Product type</label>
-                                            <select id="status" class="form-control">
-                                                <option selected>Choose...</option>
-                                                <option>...</option>
-                                            </select>
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="text" class="form-control" id="productquantity" placeholder="Product quantity (kg)">
-                                        </div>
-                                        <div class="d-flex flex-row align-items-center justify-content-between">
-                                            <a href="\app\views\Login\gleaner_feed.php">Cancel</a>
-                                            <button type= "submit" class="btn btn-primary">Create a gleaning event</button>
-                                        </div>
-                                    </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </main>
-        <?php $this->view('Layout/Footer'); ?>
-        <?php $this->view('Layout/Scripts'); ?>
-    </body>
-
+<html>
+<head>
+    <?php
+    $this->view('Layout/Head');
+    ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link
+    rel="canonical"
+    href="https://getbootstrap.com/docs/5.2/examples/carousel/"
+    />
+    <link
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
+    rel="stylesheet"
+    integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi"
+    crossorigin="anonymous"
+    /> 
+    <!-- <link href="../styles/bootstrap.min.css" rel="stylesheet" /> -->
     <style>
-        @import url('https://fonts.googleapis.com/css?family=PT+Sans');
-
-        body{
-            background: #fff;
-            font-family: 'PT Sans', sans-serif;
-        }
-        h2{
-            padding-top: 1.5rem;
-        }
-        .card{
-            border: 0.40rem solid #f8f9fa;
-            top: 10%;
-        }
-        .form-control{
-            background-color: #f8f9fa;
-            padding: 20px;
-            padding: 25px 15px;
-            margin-bottom: 1.3rem;
+        body {
+        font-family: Arial, Helvetica, sans-serif;
         }
 
-        .form-control:focus {
-            color: #000000;
-            background-color: #ffffff;
-            border: 3px solid #da5767;
-            outline: 0;
-            box-shadow: none;
+        * {
+        box-sizing: border-box;
         }
-        .btn{
-            padding: 0.6rem 1.2rem;
-            background: #50c2d1;
-            border: 2px solid #50c2d1;
+
+        /* Style inputs */
+        input[type=text], select, textarea {
+        width: 100%;
+        padding: 12px;
+        border: 1px solid #ccc;
+        margin-top: 6px;
+        margin-bottom: 16px;
+        resize: vertical;
         }
-        .btn-primary:hover {
-            background-color: #00D100;
-            border-color: #00D100;
-            transition: .3s;
+
+        input[type=submit] {
+        background-color: #1bbac5;
+        color: white;
+        padding: 12px 20px;
+        border: none;
+        cursor: pointer;
+        }
+
+        input[type=submit]:hover {
+        background-color: #05a098;
+        }
+
+        /* Style the container/contact section */
+        .container {
+        border-radius: 5px;
+        background-color: #f2f2f2;
+        padding: 10px;
+        }
+
+        /* Create two columns that float next to eachother */
+        .column {
+        float: left;
+        width: 50%;
+        margin-top: 6px;
+        padding: 20px;
+        }
+
+        /* Clear floats after the columns */
+        .row:after {
+        content: "";
+        display: table;
+        clear: both;
+        }
+
+        /* Responsive layout - when the screen is less than 600px wide, make the two columns stack on top of each other instead of next to each other */
+        @media screen and (max-width: 600px) {
+        .column, input[type=submit] {
+            width: 100%;
+            margin-top: 0;
+        }
         }
     </style>
+</head>
+
+<body>
+<?php $this->view('Layout/Navigation'); ?>
+    <main>
+        <div class="container">
+        <div style="text-align:center">
+            <h2>Create an Event</h2>
+        </div>
+        <div class="row">
+            
+            <div class="column">
+            <br>
+            <br>
+            <img src="/resources/images/eventearth.jpg" style="width:100%">
+            </div>
+            <div class="column">
+            <form action="/action_page.php">
+                <label for="fname">Name</label>
+                <input type="text" id="fname" name="name" placeholder="Name">
+
+                <label for="urgency">Urgency</label>
+                <select id="urgency" name="urgency">
+                <option value="High">High</option>
+                <option value="Moderate">Moderate</option>
+                <option value="Low">Low</option>
+                </select>
+
+                <label for="fname">Produce Quantity</label>
+                <input type="text" id="pquantity" name="quantity" placeholder="Produce Quantity">
+
+                <label for="subject">Info</label>
+                <textarea id="subject" name="info" placeholder="Write something.." style="height:170px"></textarea>
+                
+                <input type="submit" value="Cancel">
+                
+                <input type="submit" value="Submit">
+            </form>
+            </div>
+        </div>
+        </div>
+    </main>
+    <?php $this->view('Layout/Scripts'); ?>
+    
+</body>
 </html>
