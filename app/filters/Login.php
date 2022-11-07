f079af1fffd2dce7acac512b59df8a8bb1fb3be5
Md Saqliyan Islam <82405606+SaqSaq815@users.noreply.github.com>
2022-11-07 09:48:19 -0500

    fixed minor
    added location

--- app/filters/Login.php
+++ app/filters/Login.php
@@ -5,7 +5,7 @@ namespace app\filters;
 class Login extends \app\core\AccessFilter{
     public function execute(){
         if(!isset($_SESSION['account_id'])){
-            header('location:/?error=You must be logged in to access this page');
+            header('location:/Login/gleanerLogin?error=You must be logged in to access this page');
             return true;
         }
         return false;
